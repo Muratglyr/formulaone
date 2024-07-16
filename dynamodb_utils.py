ea2a3986e90cfa62d974cfe7026ec54fff9f4303
Murat Gölyeri <64622124+Muratglyr@users.noreply.github.com>
2024-07-16 16:37:38 +0200

    Update dynamodb_utils.py

--- dynamodb_utils.py
+++ dynamodb_utils.py
@@ -1,9 +1,21 @@
 import boto3
 import pandas as pd
+import json
 
-def load_data_from_dynamodb(table_name, region_name='us-west-2'):
-    dynamodb = boto3.resource('dynamodb', region_name=region_name)
-    table = dynamodb.Table(table_name)
+def load_config(config_path='config.json'):
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+def load_data_from_dynamodb():
+    config = load_config()
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=config['aws_access_key_id'],
+        aws_secret_access_key=config['aws_secret_access_key'],
+        region_name=config['region_name']
+    )
+    table = dynamodb.Table(config['table_name'])
     response = table.scan()
     data = response['Items']
     df = pd.DataFrame(data)
